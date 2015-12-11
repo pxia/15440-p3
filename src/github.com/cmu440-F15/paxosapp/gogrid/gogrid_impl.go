9e1763e2b95a0274da86ffc43723c6b7598627a9
Peter Xia <aattyy@gmail.com>
2015-12-11 04:47:10 +0000

    add /api/cellchange

--- src/github.com/cmu440-F15/paxosapp/gogrid/gogrid_impl.go
+++ src/github.com/cmu440-F15/paxosapp/gogrid/gogrid_impl.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	// "github.com/cmu440-F15/paxosapp/paxos"
+	"github.com/cmu440-F15/paxosapp/paxos"
+	"github.com/cmu440-F15/paxosapp/rpc/paxosrpc"
 	// "net"
 	"encoding/json"
 	"flag"
 	"net/http"
 	"strconv"
-)
-
-var (
-	staticDir  = flag.String("static", "", "directory to static files")
-	serverPort = flag.Uint("port", 10086, "port to to access the web app")
-	paxoPort   = flag.Uint("paxoport", 10087, "port to communicate with other nodes")
+	"strings"
+	"sync"
+	"time"
 )
 
 type gridData struct {
@@ -22,9 +20,14 @@ type gridData struct {
 	Data [][]string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, r.URL.Path)
-	grid := gridData{
+var (
+	staticDir  = flag.String("static", "", "directory to static files")
+	serverPort = flag.Uint("port", 10086, "port to to access the web app")
+	// paxoPort   = flag.Uint("paxoport", 10087, "port to communicate with other nodes")
+	paxoNodes = flag.String("paxonodes", "peterxia.com:10087,peterxia.com:10089", "hostports (containing self) seperated by commas")
+	srvId     = flag.Int("srvid", -1, "srvId")
+	paxosNode paxos.PaxosNode
+	grid      = gridData{
 		Rows: 3,
 		Cols: 3,
 		Data: [][]string{
@@ -33,7 +36,55 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			[]string{"f", "g", "h"},
 		},
 	}
+	gridLock = &sync.RWMutex{}
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&grid)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, r.URL.Path)
+	// http.Handle("/", http.FileServer(http.Dir("/")))
+}
+
+type cellChangeReq struct {
+	Row   int
+	Col   int
+	Value string
+}
+
+func cellchangeHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		return
+	}
+	r.ParseForm()
+	// fmt.Println(r.Form)
+	// fmt.Println(r.PostForm)
+}
+
+func doUpdate() {
+	c := time.NewTicker(time.Duration(5) * time.Second).C
+	for {
+		for r := 0; r < grid.Rows; r++ {
+			for c := 0; c < grid.Cols; c++ {
+				args := paxosrpc.GetValueArgs{
+					Key: fmt.Sprintf("%d,%d", r, c),
+				}
+				reply := paxosrpc.GetValueReply{}
+				if err := paxosNode.GetValue(&args, &reply); err != nil {
+					continue
+				}
+
+				if reply.V != nil {
+					gridLock.Lock()
+					grid.Data[r][c] = reply.V.(string)
+					gridLock.Unlock()
+				}
+			}
+		}
+		<-c
+	}
 }
 
 func main() {
@@ -43,10 +94,31 @@ func main() {
 		return
 	}
 
+	if *srvId < 0 {
+		fmt.Println("Please provide a srvid.")
+		return
+	}
+
+	hostportsSlice := strings.Split(*paxoNodes, ",")
+	hostports := make(map[int]string)
+	for i, hp := range hostportsSlice {
+		hostports[i] = hp
+	}
+
+	// paxosnode, err := paxos.NewPaxosNode(hostports[*srvId], hostports, len(hostports), *srvId, 60, false)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// paxosNode = paxosnode
+
 	http.HandleFunc("/api/data", handler)
+	http.HandleFunc("/api/cellchange", cellchangeHandle)
+	http.HandleFunc("/api/test", testHandler)
 
 	fs := http.FileServer(http.Dir(*staticDir))
 	http.Handle("/", fs)
-	// http.HandleFunc("/", handler)
+
+	fmt.Println("Starting server on port " + strconv.FormatUint(uint64(*serverPort), 10))
 	http.ListenAndServe(":"+strconv.FormatUint(uint64(*serverPort), 10), nil)
 }
